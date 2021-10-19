b4da0b5b9482ebcd7b8ca0a3502c1108d682df2c
Dan Malec <dan.malec@gmail.com>
2021-10-19 07:36:27 -0400

    ISSUE-11: Added bat animation

---
+++ spritesheet.hpp
@@ -0,0 +1,16 @@
+// Copyright(c) 2021 Dan Malec
+//
+// MIT License (see LICENSE file for details)
+
+#ifndef SPRITESHEET_HPP_INCLUDED_
+#define SPRITESHEET_HPP_INCLUDED_
+
+
+#include "picosystem.hpp"
+
+using namespace picosystem;
+
+extern buffer_t *custom_sprite_sheet;
+
+
+#endif // SPRITESHEET_HPP_INCLUDED_
